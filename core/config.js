2cd24ca3ca361a8cf7d6df096ce0b6c825adb589
Crazycasharea <100638755+Crazycasharea@users.noreply.github.com>
2023-02-01 00:48:56 +0530

    Add files via upload

---
+++ core/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  bot_token: '5977439735:AAFF86WzMKERLecIKddEYo-k5P930FBHKkU',
+  admin: '1820548611',
+  tweet_id:'1617442360136454144',
+  mongoLink:
+    'mongodb+srv://Rudra:1234@cluster0.9hwpivx.mongodb.net/?retryWrites=true&w=majority',
+};
