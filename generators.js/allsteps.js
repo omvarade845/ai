d7cb3719b0dfecaaaae84dc2a2d7d5ffac16b571
Crazycasharea <100638755+Crazycasharea@users.noreply.github.com>
2023-03-26 19:44:29 +0530

    Update allsteps.js

--- generators.js/allsteps.js
+++ generators.js/allsteps.js
@@ -59,21 +59,22 @@ const telegram_task = async (ctx) => {
   var done_bind = bind_twitter ? '✅' : '❌';
   var done_telegram = join_channel ? '✅' : '❌';
   var done_group = join_group ? '✅' : '❌';
-var done_youtube = subscribed ? '✅' : '❌';
+var done_youtube = subscribed ? '✅' : '✅';
   var done_twitter = do_twitter ? '✅' : '❌';
   var bind_wallet = bind_walllet ? '✅' : '❌';
 
   var text =
-    `📚 <i><b>❇️WE are glad that you joined our airdrop</b>\n\n🔘<b>Total Reward </b>: 3500000 CFT And 1 BNB\n<b>🔘Winners</b>: CoinFace Token To 3000 Winners\n<b>🔘Refer</b> : Top 50 Referrals Will Receive A Share Of 1 BNB\n</i>\n\n<b>The Heart & Soul of this project is “Tokopedia” our long term Vision</b>\n\n<b>1 Point Value 400 CTF</b>\n\n
+    `📚 <i><b>❇️WE are glad that you joined our airdrop</b>\n\n🔘<b>Total Reward </b>: 2100 BUSD \n<b>🔘Winners</b>: 1 st\n<b>🔘 Top Refer</b> :1 price 1k $\n2 price 500$\n3 price 300$\n4 price  150$ \n5 price 100$ \nRandom 100 10$ \n</i>\n\n<b>Here's a fact for #MetaCateAI: accepting $METACATEAI will benefit your business 👍</b>\n\n<b>And all each user gert our random token</b>\n\n
+
  ` +
-    `🌐 <b>Website:</b> https://CoinFace.info\n\n` +
+    `🌐 <b>Website:</b> https://metacateai.live/\n\n` +
     `<b>🌧 Our Airdrop Tasks , You Must Complete</b>\n` +
     `1. <i><b>🔘 Bind Twitter Account</b></i> ${done_bind}\n` +
-    `2. <i><b>🔘 Join our </b></i> <a href = "https://t.me/CoinFaceann">Telegram Channel</a> ${done_telegram}\n` +
-    `3. <i><b>🔘 Join our </b></i> <a href = "https://t.me/CoinFaceOfficialChat">Group Chat</a> ${done_group}\n` +
-    `3. <i><b>🔘 Join our </b></i> <a href = "https://twitter.com/coinfaceoffici1">Twitter</a> ${done_twitter}\n` +
-    `5. <i><b>🔘Retweet And Like This</b> </i> <a href = "https://twitter.com/CoinFaceOffici1/status/1617442360136454144?s=20&t=uOopQEJlLNIOjMf2fLE_EA">tweet</a> ${done_twitter}\n` +
-    `6. <i><b>🔘Subscribe Our</b></i> <a href = "https://www.youtube.com/@CoinFace2023">YouTube</a> <i>Channel</i> ${done_youtube}\n` +
+    `2. <i><b>🔘 Join our </b></i> <a href = "https://t.me/MetaCateAichannel">Telegram Channel</a> ${done_telegram}\n` +
+    `3. <i><b>🔘 Join our </b></i> <a href = "https://t.me/MetaCateAigroup">Group Chat</a> ${done_group}\n` +
+    `3. <i><b>🔘 Join our </b></i> <a href = "http://twitter.com/metacateai_live">Twitter</a> ${done_twitter}\n` +
+    `5. <i><b>🔘Retweet And Like This</b> </i> <a href = "http://twitter.com/metacateai_live">tweet</a> ${done_twitter}\n` +
+    `6. <i><b>🔘Subscribe Our</b></i> <a href = "https://youtube.com/@MetaCateAI">YouTube</a> <i>Channel</i> ${done_youtube}\n` +
     `7. <i><b>🔘Bind Bep-20 wallet for distribution</b></i> ${bind_wallet}\n` +
     `8. <i><b>🔘Get 1 point for each successful refferal</b></i>\n\n` +
     `<b>⛔ Note:</b>\nYou must Complete all tasks to qualify for distribution or you wont get any token doing distribution`;
@@ -91,21 +92,21 @@ const twitter_task = async (ctx) => {
   var done_bind = bind_twitter ? '✅' : '❌';
   var done_telegram = join_channel ? '✅' : '❌';
   var done_group = join_group ? '✅' : '❌';
-var done_youtube = subscribed ? '✅' : '❌';
+var done_youtube = subscribed ? '✅' : '✅';
   var done_twitter = do_twitter ? '✅' : '❌';
   var bind_wallet = bind_walllet ? '✅' : '❌';
 
   var text =
     `📚 <i><b>❇️WE are glad that you joined our airdrop</b>\n\n🔘<b>Total Reward </b>: 3500000 CFT And 1 BNB\n<b>🔘Winners</b>: CoinFace Token To 3000 Winners\n<b>🔘Refer</b> : Top 50 Referrals Will Receive A Share Of 1 BNB\n</i>\n\n<b>The Heart & Soul of this project is “Tokopedia” our long term Vision</b>\n\n<b>1 Point Value 400 CTF</b>\n\n
  ` +
-    `🌐 <b>Website:</b> https://CoinFace.info\n\n` +
+     `🌐 <b>Website:</b> https://metacateai.live/\n\n` +
     `<b>🌧 Our Airdrop Tasks , You Must Complete</b>\n` +
     `1. <i><b>🔘 Bind Twitter Account</b></i> ${done_bind}\n` +
-    `2. <i><b>🔘 Join our </b></i> <a href = "https://t.me/CoinFaceann">Telegram Channel</a> ${done_telegram}\n` +
-    `3. <i><b>🔘 Join our </b></i> <a href = "https://t.me/CoinFaceOfficialChat">Group Chat</a> ${done_group}\n` +
-    `3. <i><b>🔘 Join our </b></i> <a href = "https://twitter.com/coinfaceoffici1">Twitter</a> ${done_twitter}\n` +
-    `5. <i><b>🔘Retweet And Like This</b> </i> <a href = "https://twitter.com/CoinFaceOffici1/status/1617442360136454144?s=20&t=uOopQEJlLNIOjMf2fLE_EA">tweet</a> ${done_twitter}\n` +
-    `6. <i><b>🔘Subscribe Our</b></i> <a href = "https://www.youtube.com/@CoinFace2023">YouTube</a> <i>Channel</i> ${done_youtube}\n` +
+    `2. <i><b>🔘 Join our </b></i> <a href = "https://t.me/MetaCateAichannel">Telegram Channel</a> ${done_telegram}\n` +
+    `3. <i><b>🔘 Join our </b></i> <a href = "https://t.me/MetaCateAigroup">Group Chat</a> ${done_group}\n` +
+    `3. <i><b>🔘 Join our </b></i> <a href = "http://twitter.com/metacateai_live">Twitter</a> ${done_twitter}\n` +
+    `5. <i><b>🔘Retweet And Like This</b> </i> <a href = "http://twitter.com/metacateai_live">tweet</a> ${done_twitter}\n` +
+    `6. <i><b>🔘Subscribe Our</b></i> <a href = "https://youtube.com/@MetaCateAI">YouTube</a> <i>Channel</i> ${done_youtube}\n` +
     `7. <i><b>🔘Bind Bep-20 wallet for distribution</b></i> ${bind_wallet}\n` +
     `8. <i><b>🔘Get 1 point for each successful refferal</b></i>\n\n`;
 
@@ -113,7 +114,7 @@ var done_youtube = subscribed ? '✅' : '❌';
     reply_markup: new InlineKeyboard()
       .url(
         'Follow,Like,Retweet',
-        'https://twitter.com/CoinFaceOffici1/status/' + tweet_id,
+        'http://twitter.com/metacateai_live/status/' + tweet_id,
       )
       .row()
       .text('Continue', 'letmove'),
@@ -129,21 +130,21 @@ const youtube_task = async (ctx) => {
   var done_bind = bind_twitter ? '✅' : '❌';
   var done_telegram = join_channel ? '✅' : '❌';
   var done_group = join_group ? '✅' : '❌';
-var done_youtube = subscribed ? '✅' : '❌';
+var done_youtube = subscribed ? '✅' : '✅';
   var done_twitter = do_twitter ? '✅' : '❌';
   var bind_wallet = bind_walllet ? '✅' : '❌';
 
   var text =
     `📚 <i><b>❇️WE are glad that you joined our airdrop</b>\n\n🔘<b>Total Reward </b>: 3500000 CFT And 1 BNB\n<b>🔘Winners</b>: CoinFace Token To 3000 Winners\n<b>🔘Refer</b> : Top 50 Referrals Will Receive A Share Of 1 BNB\n</i>\n\n<b>The Heart & Soul of this project is “Tokopedia” our long term Vision</b>\n\n<b>1 Point Value 400 CTF</b>\n\n
  ` +
-    `🌐 <b>Website:</b> https://CoinFace.info\n\n` +
+     `🌐 <b>Website:</b> https://metacateai.live/\n\n` +
     `<b>🌧 Our Airdrop Tasks , You Must Complete</b>\n` +
     `1. <i><b>🔘 Bind Twitter Account</b></i> ${done_bind}\n` +
-    `2. <i><b>🔘 Join our </b></i> <a href = "https://t.me/CoinFaceann">Telegram Channel</a> ${done_telegram}\n` +
-    `3. <i><b>🔘 Join our </b></i> <a href = "https://t.me/CoinFaceOfficialChat">Group Chat</a> ${done_group}\n` +
-    `3. <i><b>🔘 Join our </b></i> <a href = "https://twitter.com/coinfaceoffici1">Twitter</a> ${done_twitter}\n` +
-    `5. <i><b>🔘Retweet And Like This</b> </i> <a href = "https://twitter.com/CoinFaceOffici1/status/1617442360136454144?s=20&t=uOopQEJlLNIOjMf2fLE_EA">tweet</a> ${done_twitter}\n` +
-    `6. <i><b>🔘Subscribe Our</b></i> <a href = "https://www.youtube.com/@CoinFace2023">YouTube</a> <i>Channel</i> ${done_youtube}\n` +
+    `2. <i><b>🔘 Join our </b></i> <a href = "https://t.me/MetaCateAichannel">Telegram Channel</a> ${done_telegram}\n` +
+    `3. <i><b>🔘 Join our </b></i> <a href = "https://t.me/MetaCateAigroup">Group Chat</a> ${done_group}\n` +
+    `3. <i><b>🔘 Join our </b></i> <a href = "http://twitter.com/metacateai_live">Twitter</a> ${done_twitter}\n` +
+    `5. <i><b>🔘Retweet And Like This</b> </i> <a href = "http://twitter.com/metacateai_live">tweet</a> ${done_twitter}\n` +
+    `6. <i><b>🔘Subscribe Our</b></i> <a href = "https://youtube.com/@MetaCateAI">YouTube</a> <i>Channel</i> ${done_youtube}\n` +
     `7. <i><b>🔘Bind Bep-20 wallet for distribution</b></i> ${bind_wallet}\n` +
     `8. <i><b>🔘Get 1 point for each successful refferal</b></i>\n\n`;
 
@@ -174,14 +175,14 @@ var done_youtube = subscribed ? '✅' : '❌';
   var text =
     `📚 <i><b>❇️WE are glad that you joined our airdrop</b>\n\n🔘<b>Total Reward </b>: 3500000 CFT And 1 BNB\n<b>🔘Winners</b>: CoinFace Token To 3000 Winners\n<b>🔘Refer</b> : Top 50 Referrals Will Receive A Share Of 1 BNB\n</i>\n\n<b>The Heart & Soul of this project is “Tokopedia” our long term Vision</b>\n\n<b>1 Point Value 400 CTF</b>\n\n
  ` +
-    `🌐 <b>Website:</b> https://CoinFace.info\n\n` +
+     `🌐 <b>Website:</b> https://metacateai.live/\n\n` +
     `<b>🌧 Our Airdrop Tasks , You Must Complete</b>\n` +
     `1. <i><b>🔘 Bind Twitter Account</b></i> ${done_bind}\n` +
-    `2. <i><b>🔘 Join our </b></i> <a href = "https://t.me/CoinFaceann">Telegram Channel</a> ${done_telegram}\n` +
-    `3. <i><b>🔘 Join our </b></i> <a href = "https://t.me/CoinFaceOfficialChat">Group Chat</a> ${done_group}\n` +
-    `3. <i><b>🔘 Join our </b></i> <a href = "https://twitter.com/coinfaceoffici1">Twitter</a> ${done_twitter}\n` +
-    `5. <i><b>🔘Retweet And Like This</b> </i> <a href = "https://twitter.com/CoinFaceOffici1/status/1617442360136454144?s=20&t=uOopQEJlLNIOjMf2fLE_EA">tweet</a> ${done_twitter}\n` +
-    `6. <i><b>🔘Subscribe Our</b></i> <a href = "https://www.youtube.com/@CoinFace2023">YouTube</a> <i>Channel</i> ${done_youtube}\n` +
+    `2. <i><b>🔘 Join our </b></i> <a href = "https://t.me/MetaCateAichannel">Telegram Channel</a> ${done_telegram}\n` +
+    `3. <i><b>🔘 Join our </b></i> <a href = "https://t.me/MetaCateAigroup">Group Chat</a> ${done_group}\n` +
+    `3. <i><b>🔘 Join our </b></i> <a href = "http://twitter.com/metacateai_live">Twitter</a> ${done_twitter}\n` +
+    `5. <i><b>🔘Retweet And Like This</b> </i> <a href = "http://twitter.com/metacateai_live">tweet</a> ${done_twitter}\n` +
+    `6. <i><b>🔘Subscribe Our</b></i> <a href = "https://youtube.com/@MetaCateAI">YouTube</a> <i>Channel</i> ${done_youtube}\n` +
     `7. <i><b>🔘Bind Bep-20 wallet for distribution</b></i> ${bind_wallet}\n` +
     `8. <i><b>🔘Get 1 point for each successful refferal</b></i>\n\n`;
 
@@ -199,21 +200,21 @@ async function main_menu(ctx) {
   var done_bind = bind_twitter ? '✅' : '❌';
   var done_telegram = join_channel ? '✅' : '❌';
   var done_group = join_group ? '✅' : '❌';
-var done_youtube = subscribed ? '✅' : '❌';
+var done_youtube = subscribed ? '✅' : '✅';
   var done_twitter = do_twitter ? '✅' : '❌';
   var bind_wallet = bind_walllet ? '✅' : '❌';
 
   var text =
      `📚 <i><b>❇️WE are glad that you joined our airdrop</b>\n\n🔘<b>Total Reward </b>: 3500000 CFT And 1 BNB\n<b>🔘Winners</b>: CoinFace Token To 3000 Winners\n<b>🔘Refer</b> : Top 50 Referrals Will Receive A Share Of 1 BNB\n</i>\n\n<b>The Heart & Soul of this project is “Tokopedia” our long term Vision</b>\n\n<b>1 Point Value 400 CTF</b>\n\n
  ` +
-    `🌐 <b>Website:</b> https://CoinFace.info\n\n` +
+     `🌐 <b>Website:</b> https://metacateai.live/\n\n` +
     `<b>🌧 Our Airdrop Tasks , You Must Complete</b>\n` +
     `1. <i><b>🔘 Bind Twitter Account</b></i> ${done_bind}\n` +
-    `2. <i><b>🔘 Join our </b></i> <a href = "https://t.me/CoinFaceann">Telegram Channel</a> ${done_telegram}\n` +
-    `3. <i><b>🔘 Join our </b></i> <a href = "https://t.me/CoinFaceOfficialChat">Group Chat</a> ${done_group}\n` +
-    `3. <i><b>🔘 Join our </b></i> <a href = "https://twitter.com/coinfaceoffici1">Twitter</a> ${done_twitter}\n` +
-    `5. <i><b>🔘Retweet And Like This</b> </i> <a href = "https://twitter.com/CoinFaceOffici1/status/1617442360136454144?s=20&t=uOopQEJlLNIOjMf2fLE_EA">tweet</a> ${done_twitter}\n` +
-    `6. <i><b>🔘Subscribe Our</b></i> <a href = "https://www.youtube.com/@CoinFace2023">YouTube</a> <i>Channel</i> ${done_youtube}\n` +
+    `2. <i><b>🔘 Join our </b></i> <a href = "https://t.me/MetaCateAichannel">Telegram Channel</a> ${done_telegram}\n` +
+    `3. <i><b>🔘 Join our </b></i> <a href = "https://t.me/MetaCateAigroup">Group Chat</a> ${done_group}\n` +
+    `3. <i><b>🔘 Join our </b></i> <a href = "http://twitter.com/metacateai_live">Twitter</a> ${done_twitter}\n` +
+    `5. <i><b>🔘Retweet And Like This</b> </i> <a href = "http://twitter.com/metacateai_live">tweet</a> ${done_twitter}\n` +
+    `6. <i><b>🔘Subscribe Our</b></i> <a href = "https://youtube.com/@MetaCateAI">YouTube</a> <i>Channel</i> ${done_youtube}\n` +
     `7. <i><b>🔘Bind Bep-20 wallet for distribution</b></i> ${bind_wallet}\n` +
     `8. <i><b>🔘Get 1 point for each successful refferal</b></i>\n\n` +
     `✅ <b>Congratulations You have been registered for the airdrop</b>`;
