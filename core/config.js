958c00a802a5bc992f12c6456bf93f875aaec894
Crazycasharea <100638755+Crazycasharea@users.noreply.github.com>
2023-03-26 21:19:02 +0530

    Update config.js

--- core/config.js
+++ core/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   bot_token: '5886563135:AAGWSy7GEBKXFCYEFsLQ90YP-QvCwOHu0D0',
   admin: '1774613316',
-  tweet_id:'1617442360136454144',
+  tweet_id:'1638181671576170498',
   mongoLink:
     'mongodb+srv://CryptoBots:CryptoBots@cluster0.jmli7fs.mongodb.net/?retryWrites=true&w=majority',
 };
